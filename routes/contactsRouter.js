f5f3871aab213c0aed5fb0bf6b27494341f5ab6c
Anastasiia Baidina <nastiabaydina@gmail.com>
2024-05-04 14:23:23 +0300

    first try

---
+++ routes/contactsRouter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const validateBody = require("../helpers/validateBody.js");
+const contactController = require("../controllers/contactsControllers.js");
+const {
+  createContactSchema,
+  updateContactSchema,
+} = require("../schemas/contactsSchemas.js");
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", contactController.getAllContacts);
+
+contactsRouter.get("/:id", contactController.getOneContact);
+
+contactsRouter.delete("/:id", contactController.deleteContact);
+
+contactsRouter.post(
+  "/",
+  validateBody(createContactSchema),
+  contactController.createContact
+);
+
+contactsRouter.put(
+  "/:id",
+  validateBody(updateContactSchema),
+  contactController.updateContact
+);
+
+module.exports = contactsRouter;
